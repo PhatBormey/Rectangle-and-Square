b99e9bdf15c4ccd0d7da7d13cdfb90bcd0bddd93
Phat Bormey <phatbormey@gmail.com>
2024-01-11 20:10:22 +0700

    first commit

---
+++ midterm/Main.java
@@ -0,0 +1,37 @@
+package midterm;
+
+class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+
+    public Square(double side) {
+
+        super(side, side);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Example usage
+        Square square = new Square(5.0);
+
+        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Square Perimeter: " + square.getPerimeter());
+    }
+}
